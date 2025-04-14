fd0175d9531409691b7d464e49829732ec42188e
David Dixon <david.dixon@amd.com>
2025-04-14 18:22:41 +0000

    Revert "Use sub_directory instead"
    This reverts commit 488cbe0850369de6d49a0fbb9609833dc77fe3ad.

--- tensilelite/Tensile/cmake/TensileConfig.cmake
+++ tensilelite/Tensile/cmake/TensileConfig.cmake
@@ -24,6 +24,10 @@
 
 include(CMakeParseArguments)
 
+if(NOT Rocisa_FOUND)
+    find_package(Rocisa 0.1.0 EXACT REQUIRED)
+endif()
+
 if(NOT DEFINED Tensile_ROOT)
     # Compute the installation prefix relative to this file.
     get_filename_component(Tensile_PREFIX "${CMAKE_CURRENT_LIST_FILE}" PATH)
@@ -72,10 +76,10 @@ endif()
 add_subdirectory("${Tensile_ROOT}/Source" "Tensile")
 include("${Tensile_ROOT}/Source/TensileCreateLibrary.cmake")
 
+# Output target: ${Tensile_VAR_PREFIX}_LIBRARY_TARGET. Ensures that the libs get built in Tensile_OUTPUT_PATH/library.
 function(TensileCreateLibraryFiles
          Tensile_LOGIC_PATH
          Tensile_OUTPUT_PATH
-         Tensile_LIBRARY_TARGET
          )
 
   # Boolean options
@@ -209,7 +213,7 @@ function(TensileCreateLibraryFiles
     set(Options ${Options} "--build-id=${Tensile_BUILD_ID}")
   endif()
 
-  set(CommandLine ${CMAKE_COMMAND} -E env PYTHONPATH=${PROJECT_BINARY_DIR}/lib -- ${VIRTUALENV_BIN_DIR}/${VIRTUALENV_PYTHON_EXENAME} ${Script} ${Options} ${Tensile_LOGIC_PATH} ${Tensile_OUTPUT_PATH} HIP)
+  set(CommandLine ${CMAKE_COMMAND} -E env PYTHONPATH=${Rocisa_LIBRARIES} -- ${VIRTUALENV_BIN_DIR}/${VIRTUALENV_PYTHON_EXENAME} ${Script} ${Options} ${Tensile_LOGIC_PATH} ${Tensile_OUTPUT_PATH} HIP)
   message(STATUS "Tensile_CREATE_COMMAND: ${CommandLine}")
 
   if(Tensile_EMBED_LIBRARY)
@@ -219,6 +223,10 @@ function(TensileCreateLibraryFiles
   if(Tensile_SKIP_BUILD)
       message(STATUS "Skipping build of ${Tensile_OUTPUT_PATH}")
   else()
+      if(NOT Tensile_VAR_PREFIX)
+          set(Tensile_VAR_PREFIX TENSILE)
+      endif()
+
       if($ENV{ENABLE_ADDRESS_SANITIZER})
         # Must populate LD_PRELOAD with ASAN runtime if ASAN is being used.
         # Find the ASAN RT with compiler and update env for Tensile call.
@@ -237,8 +245,8 @@ function(TensileCreateLibraryFiles
       )
 
       add_custom_target(
-        ${Tensile_LIBRARY_TARGET} ALL
-        COMMENT "${Tensile_LIBRARY_TARGET}"
+        "${Tensile_VAR_PREFIX}_LIBRARY_TARGET" ALL
+        COMMENT "${Tensile_VAR_PREFIX}_LIBRARY_TARGET"
         DEPENDS ${Tensile_OUTPUT_PATH}/library
       )
 
@@ -253,7 +261,7 @@ function(TensileCreateLibraryFiles
 
 endfunction()
 
-function(TensileCreateExtOpLibraries OutputFolder ArchStr TensileExt_LIBRARY_TARGET)
+function(TensileCreateExtOpLibraries OutputFolder ArchStr)
   string(REGEX MATCHALL "gfx[a-z0-9]+" Archs "${ArchStr}")
   list(REMOVE_DUPLICATES Archs)
   set(build_tmp_dir ${OutputFolder}/../build_tmp/ops)
@@ -270,12 +278,12 @@ function(TensileCreateExtOpLibraries OutputFolder ArchStr TensileExt_LIBRARY_TAR
     COMMAND ${CMAKE_COMMAND} -E rm -rf ${build_tmp_dir}
     COMMAND ${CMAKE_COMMAND} -E make_directory ${build_tmp_dir}
     COMMAND ${CMAKE_COMMAND} -E make_directory ${OutputFolder}
-    COMMAND ${CMAKE_COMMAND} -E env PYTHONPATH=${PROJECT_BINARY_DIR}/lib -- bash "${script}" "\"${Archs}\"" "${build_tmp_dir}" "${VIRTUALENV_HOME_DIR}" "${Tensile_BUILD_ID}"
+    COMMAND ${CMAKE_COMMAND} -E env PYTHONPATH=${Rocisa_LIBRARIES} -- bash "${script}" "\"${Archs}\"" "${build_tmp_dir}" "${VIRTUALENV_HOME_DIR}" "${Tensile_BUILD_ID}"
     COMMAND ${CMAKE_COMMAND} -E copy ${ext_op_library_path} ${build_tmp_dir}/extop_*.co ${OutputFolder}
   )
 
   add_custom_target(
-    ${TensileExt_LIBRARY_TARGET} ALL
+    build_ext_op_library ALL
     WORKING_DIRECTORY "${cwd}"
     DEPENDS ${OutputFolder}/hipblasltExtOpLibrary.dat)
 
